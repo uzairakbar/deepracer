8fec55858d910d35de4a31d5224c43f646e4a497
Uzair Akbar <uzair.akbar@gatech.edu>
2025-03-19 20:14:05 -0400

    Use command line args for docker container.

--- scripts/start_deepracer.sh
+++ scripts/start_deepracer.sh
@@ -46,6 +46,6 @@ docker run -it --rm \
     --name="$container" \
     -v "$PWD"/"$logs":/"$logs" \
     -p 8888:8888 -p 5000:5000 \
-    --cpus="3" --memory="6g" \
+    --cpus="$cpus" --memory="$memory" \
     "$image" \
     /bin/bash
